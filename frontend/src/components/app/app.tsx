4ebcdd4375e75704dbac0805e5e8a375ce9df731
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-30 23:34:47 +0300

    Добавляет компонент страницы редактирования товара

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 //import ProductCardPage from '../../pages/product-card-page/product-card-page';
 //import ProductListPage from '../../pages/product-list-page/product-list-page';
-import AddProductPage from '../../pages/add-product-page/add-product-page';
+//import AddProductPage from '../../pages/add-product-page/add-product-page';
+import EditProductPage from '../../pages/edit-product-page/edit-product-page';
 
 function App(): JSX.Element {
   return (
-    <AddProductPage />
+    <EditProductPage />
   );
 }
 
