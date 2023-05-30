f5e1ce57583234ec250bf0540598d0f7d585184c
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-30 23:39:05 +0300

    Добавляет компонент страницы 404

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,11 +1,12 @@
 //import ProductCardPage from '../../pages/product-card-page/product-card-page';
 //import ProductListPage from '../../pages/product-list-page/product-list-page';
 //import AddProductPage from '../../pages/add-product-page/add-product-page';
-import EditProductPage from '../../pages/edit-product-page/edit-product-page';
+//import EditProductPage from '../../pages/edit-product-page/edit-product-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 function App(): JSX.Element {
   return (
-    <EditProductPage />
+    <NotFoundPage />
   );
 }
 
