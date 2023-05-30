b62fddc0a686178c62da91eb8a5e5fd4d07ae68a
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-30 23:24:01 +0300

    Добавляет компонент страницы добавления товара

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,9 +1,10 @@
 //import ProductCardPage from '../../pages/product-card-page/product-card-page';
-import ProductListPage from '../../pages/product-list-page/product-list-page';
+//import ProductListPage from '../../pages/product-list-page/product-list-page';
+import AddProductPage from '../../pages/add-product-page/add-product-page';
 
 function App(): JSX.Element {
   return (
-    <ProductListPage />
+    <AddProductPage />
   );
 }
 
