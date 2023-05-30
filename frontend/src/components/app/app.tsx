59991eb279e4c2d03131d66502018b8d6eb3a5fc
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-30 23:44:41 +0300

    Добавляет компонент страницы входа

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -2,11 +2,12 @@
 //import ProductListPage from '../../pages/product-list-page/product-list-page';
 //import AddProductPage from '../../pages/add-product-page/add-product-page';
 //import EditProductPage from '../../pages/edit-product-page/edit-product-page';
-import NotFoundPage from '../../pages/not-found-page/not-found-page';
+//import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import LoginPage from '../../pages/login-page/login-page';
 
 function App(): JSX.Element {
   return (
-    <NotFoundPage />
+    <LoginPage />
   );
 }
 
