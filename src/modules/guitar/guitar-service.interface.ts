03d8895d05d427d827c7331dbd4e4ce93d99ad2d
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-06-01 14:33:38 +0300

    Расширяет интерфейс GuitarService

--- src/modules/guitar/guitar-service.interface.ts
+++ src/modules/guitar/guitar-service.interface.ts
@@ -1,8 +1,12 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { GuitarEntity } from './guitar.entity.js';
 import CreateGuitarDto from './dto/create-guitar.dto.js';
+import UpdateGuitarDto from './dto/update-guitar.dto.js';
 
 export interface GuitarServiceInterface {
   create(dto: CreateGuitarDto): Promise<DocumentType<GuitarEntity>>;
   findById(guitarId: string): Promise<DocumentType<GuitarEntity> | null>;
+  find(): Promise<DocumentType<GuitarEntity>[]>;
+  deleteById(guitarId: string): Promise<DocumentType<GuitarEntity> | null>;
+  updateById(guitarId: string, dto: UpdateGuitarDto): Promise<DocumentType<GuitarEntity> | null>;
 }
